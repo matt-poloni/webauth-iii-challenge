a00d962b65d8b847b69d81acf93849042942e826
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-08 15:58:20 -0400

    Creates index.js, server.js, & basicModel.js and prepares for routes

---
+++ api/basicModel.js
@@ -0,0 +1,20 @@
+const db = require('../data/dbConfig');
+
+module.exports = function(tbl) {
+  return {
+    get: function(val) {
+      return val
+        ? db(tbl).where(val).first()
+        : db(tbl);
+    },    
+    post: function(entry) {
+      return db(tbl).insert(entry);
+    },
+    put: function(id, changes) {
+      return db(tbl).where({id}).update(changes);
+    },
+    del: function(id) {
+      return db(tbl).where({id}).del();
+    }
+  }
+}
---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+// const registerRt = require('./register/router');
+// const loginRt = require('./login/router');
+// const usersRt = require('./users/router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+// server.use('/api/register/', registerRt);
+// server.use('/api/login/', loginRt);
+// server.use('/api/users/', usersRt);
+
+server.get('/', (req, res) => {
+  res.send("Up and running.");
+});
+
+module.exports = server;
