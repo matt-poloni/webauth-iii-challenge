8fdf465d735ceec4e945bf49654f6c9569aa8a5e
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-08 18:41:26 -0400

    Some initial setup for the react client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { NavLink, Route } from 'react-router-dom';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import Users from './components/Users';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <nav>
+            <NavLink to="/signup">Sign Up</NavLink>
+            <NavLink to="/signin">Sign In</NavLink>
+            <NavLink to="/users">Users</NavLink>
+          </nav>
+        </header>
+        <main>
+          <Route
+            path="/signup"
+            render={SignUp}
+          />
+          <Route
+            path="/signin"
+            render={SignIn}
+          />
+          <Route
+            path="/users"
+            render={Users}
+          />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
