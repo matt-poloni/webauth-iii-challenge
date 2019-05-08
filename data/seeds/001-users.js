852f6b00089f2d8086df97b3eeab446ccf66e10b
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-08 15:31:38 -0400

    Creates seeded users table via knex

---
+++ data/seeds/001-users.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcryptjs');
+
+exports.seed = function(knex, Promise) {
+  return knex('users').truncate()
+    .then(function () {
+      return knex('users').insert([
+        {
+          id: 1,
+          username: 'admin',
+          password: bcrypt.hashSync('admin', 10)
+        },
+      ]);
+    });
+};
